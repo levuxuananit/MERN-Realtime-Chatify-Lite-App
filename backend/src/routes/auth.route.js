bf4fc6ea48f38a802a5c2069d2a14d843ab951bd
LeVuXuanAn <levuxuanan.work@gmail.com>
2025-11-16 23:01:26 +0700

    rate limiting implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -6,13 +6,18 @@ import {
   updateProfile,
 } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
-router.post("/logout", logout);
-router.put("/update-profile", protectRoute, updateProfile);
+router.use(arcjetProtection);
+
+router.post("/signup", arcjetProtection, signup);
+router.post("/login", arcjetProtection, login);
+router.post("/logout", arcjetProtection, logout);
+
+router.put("/update-profile", arcjetProtection, protectRoute, updateProfile);
+
 router.get("/check", protectRoute, (req, res) => {
   res.status(200).json(req.user);
 });
